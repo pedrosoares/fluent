9ac5370d23dec4be934f23b2a8fea1651b386375
Pedro Soares <contato@pedrosoares.net>
2022-05-13 20:49:43 -0300

    Export HasOne and HasMany direct at root

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Model } from './model';
+import { HasOne } from "./has_one";
+import { HasMany } from "./has_many";
 import { Configurator } from "./configurator";
 
 const configurator = new Configurator();
 
-export { Model, configurator };
+export { Model, HasOne, HasMany, configurator };
